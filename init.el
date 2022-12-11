d11add3ff288908ece9d0936971b4684c4bae109
Shweta Kadam <shwetarkadam25@gmail.com>
2022-12-11 14:39:06 +0530

    Update init.el

--- init.el
+++ init.el
@@ -56,7 +56,7 @@
 
 ;;  (setq org-id-extra-files (find-lisp-find-files notes-org-files "\.org$"))
                                   
-  (dolist (org-file (directory-files-recursively notes-org-files "\.org$"))
+  (dolist (org-file (find-lisp-find-files  notes-org-files "\.org$"))
     (with-current-buffer (find-file org-file)
       (message (format "[build] Exporting %s" org-file))
       (org-hugo-export-wim-to-md :all-subtrees nil nil nil)))
