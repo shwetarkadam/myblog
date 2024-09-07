e65f1ffea63ecda72a371a06227e251555193196
Shweta Kadam <shwetarkadam25@gmail.com>
2024-09-07 10:56:29 +0530

    Delete assets/js directory

--- assets/js/progress.js
+++
@@ -1,6 +0,0 @@
-window.onscroll = function() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("progressBar").style.width = scrolled + "%";
-};
